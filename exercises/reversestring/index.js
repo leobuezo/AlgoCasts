8ae5765028baeacc63eb02b67398a46b74f068e0
Leo Buezo <leobuezo@gmail.com>
2024-12-05 13:01:58 -0300

    string reverse solutions

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -2,10 +2,25 @@
 // Given a string, return a new string with the reversed
 // order of characters
 // --- Examples
-//   reverse('apple') === 'leppa'
+//   reverse('apple') === 'elppa'
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+function reverse(str) {
+/*
+    let rev = new Array(str.length);
+    let arr = str.split('');
+    console.log(arr);
+
+    for(let i = 0; i < arr.length; i++) {
+        rev[(rev.length -1) - i] = arr[i];
+    }
+    console.log("rev ", rev);
+    return rev.join('');
+*/
+    const arr = str.split('');
+    arr.reverse();
+    return arr.join('');
+}
 
 module.exports = reverse;
