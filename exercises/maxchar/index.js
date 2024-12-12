cd77605679a9ae15cbbafee29b21c3d785b74bdb
Leo Buezo <leobuezo@gmail.com>
2024-12-12 15:12:26 -0300

    maxchar solution

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,29 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+    let result = {};
+    for(let el of str) {
+        // result[el] = result[el] + 1 || 1;
+        if(!result[el]) {
+            result[el] = 1;
+        } else {
+            result[el]++;
+        }
+    }
 
+    let maxVal = 0;
+    let maxEl = null;
+    for(let prop in result) {
+        if(result[prop] > maxVal) {
+            maxVal = result[prop]
+            maxEl = prop
+        }
+    }
+
+    console.log(maxEl)
+    return maxEl;
+}
+
+maxChar("hello there!");
 module.exports = maxChar;
--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -8,6 +8,37 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+function reverseInt(n) {
+    let arr = n.toString().split('');
+    let nSign = Math.sign(n);
+    let cero = true;
+    let value = 0;
+    if(nSign === -1) {
+        arr.shift();
+    }
+    arr.reverse();
+    while(cero === true) {
+        if(arr[0] == 0) {
+            arr.shift();
+        } else {
+            cero = false;
+        }
+    }
+    if(nSign === -1) {
+        value = '-' + arr.join('')
+    } else if(nSign === 1) {
+        value = arr.join('')
+    }
+    return parseInt(value);
+
+/*
+    const reversed = n
+        .toString()
+        .split('')
+        .reverse()
+        .join('');
+    return parseInt(reversed) * Math.sign(n);
+*/
+}
 
 module.exports = reverseInt;
