62e475b4c9f8dd413c9331eb166ff6f1326a8bd4
Leo Buezo <leobuezo@gmail.com>
2025-01-24 18:11:35 -0300

    Vowels solution

--- exercises/vowels/index.js
+++ exercises/vowels/index.js
@@ -7,6 +7,17 @@
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
 
-function vowels(str) {}
+function vowels(str) {
+    let arr = str.toLowerCase().split('');
+    let counter = 0;
+    for(let i = 0; i < arr.length; i++) {
+        if(arr[i] == 'a' || arr[i] == 'e' || arr[i] == 'i' || arr[i] == 'o' || arr[i] == 'u') {
+            counter++;
+        }
+    }
+    console.log(counter);
+    return counter;
+}
 
+vowels('HolA mundo!');
 module.exports = vowels;
