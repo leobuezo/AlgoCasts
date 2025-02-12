2e46fc5da99c1c8e46ac50641695f6ea7a2e33ea
Leo Buezo <leobuezo@gmail.com>
2025-02-12 13:27:40 -0300

    node implementation

--- exercises/tree/index.js
+++ exercises/tree/index.js
@@ -10,8 +10,34 @@
 // on the tree class.  Each method should accept a
 // function that gets called with each element in the tree
 
-class Node {}
+class Node {
+    constructor(data) {
+        this.data = data;
+        this.children = [];
+    }
 
+    add(data) {
+        const newNode = new Node(data);
+        this.children[this.children.length] = newNode;
+    }
+
+    remove(data) {
+        let i = 0;
+        let found = false;
+        while(i < this.children.length && found === false) {
+            if(data == this.children[i].data && i == this.children.length-1) {
+                this.children[i] = null;
+                this.children.length -= 1; 
+                found = true;
+            } else if(data == this.children[i].data) {
+                this.children[i] = this.children[i+1];
+                found = true;
+            }
+            i++;
+        }
+    }
+}
+  
 class Tree {}
 
 module.exports = { Tree, Node };
