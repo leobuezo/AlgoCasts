352672a079d280b194af42f3499e1121ee5e6e09
Leo Buezo <leobuezo@gmail.com>
2025-02-15 20:57:17 -0300

    levelwidth solution

--- exercises/levelwidth/index.js
+++ exercises/levelwidth/index.js
@@ -10,7 +10,28 @@
 // |       |
 // 4       5
 // Answer: [1, 3, 2]
+const Node = require('./node');
 
-function levelWidth(root) {}
+function levelWidth(root) {
+    let arrayBF = [];
+    let counter = [];
+    const stopper = new Node('S');
+    arrayBF.push(root);
+    arrayBF.push(stopper);
+    let i = 0;
+    counter[i] = 0;
+    while(arrayBF.length > 1) {
+        const node = arrayBF.shift();
+        if(node.data !== stopper.data) {
+            arrayBF.push(... node.children);
+            counter[i] += 1;    
+        } else {
+            arrayBF.push(node);
+            i++;
+            counter[i] = 0;
+        }
+    }
+    return counter;
+}
 
 module.exports = levelWidth;
