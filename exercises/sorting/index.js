6783bd82c6b960f096b8f8abbecde58d9efe2356
Leo Buezo <leobuezo@gmail.com>
2025-02-21 17:50:53 -0300

    bubble sort solution

--- exercises/sorting/index.js
+++ exercises/sorting/index.js
@@ -2,19 +2,26 @@
 // Implement bubbleSort, selectionSort, and mergeSort
 
 function bubbleSort(arr) {
-
+    let sorted = false;
+    while(sorted == false) {
+        aChange = false;
+        for(let i = 0; i < arr.length -1; i++) {
+            if(arr[i] > arr[i+1]) {
+                let flag = arr[i];
+                arr[i] = arr[i+1];
+                arr[i+1] = flag;
+                aChange = true;
+            }
+        }
+        sorted = (aChange == false) ? true : false;
+    }
+    return arr;
 }
 
-function selectionSort(arr) {
+function selectionSort(arr) {}
 
-}
+function mergeSort(arr) {}
 
-function mergeSort(arr) {
-
-}
-
-function merge(left, right) {
-
-}
+function merge(left, right) {}
 
 module.exports = { bubbleSort, selectionSort, mergeSort, merge };
